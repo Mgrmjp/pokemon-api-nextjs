f9e2fd91fae8e4502e894df9a03cf7c1b596f2f1
Miikka <miikka.makela@zaibatsu.fi>
2020-11-05 13:10:06 +0200

    Show two evolutions for a Pokemon.

--- components/evolution.js
+++ components/evolution.js
@@ -1,66 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import styles from "../css/styles.module.css";
 
 export default function Evolution(props) {
+  const [evolve, setEvolve] = useState([]);
 
-console.log(props.pokemonId)
-
-const [evolve, setEvolve] = useState([])
-
-useEffect(() => {
+  useEffect(() => {
     if (props.pokemonId !== 0) {
-        fetch('https://pokeapi.co/api/v2/pokemon-species/' + props.pokemonId)
-        .then(
-            function(response) {
-            if (response.status !== 200) {
-                console.log('Looks like there was a problem. Status Code: ' +
-                response.status);
-                return;
-            }
-            response.json().then(function(data) {
-                console.log(data)
-                apiCall(data.evolution_chain.url)
-            });
-            }
-        )
-        .catch(function(err) {
-            console.log('Fetch Error :-S', err);
+      fetch("https://pokeapi.co/api/v2/pokemon-species/" + props.pokemonId)
+        .then(function (response) {
+          if (response.status !== 200) {
+            console.log(
+              "Looks like there was a problem. Status Code: " + response.status
+            );
+            return;
+          }
+          response.json().then(function (data) {
+            apiCall(data.evolution_chain.url);
+          });
+        })
+        .catch(function (err) {
+          console.log("Fetch Error :-S", err);
         });
     }
-}, [props.pokemonId]);
+  }, [props.pokemonId]);
 
-
-const apiCall = (evoUrl) => {
+  const apiCall = (evoUrl) => {
     fetch(evoUrl)
-    .then(
-      function(response) {
+      .then(function (response) {
         if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
+          console.log(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
           return;
         }
-        response.json().then(function(data) {
-            console.log(data.chain.evolves_to[0].species.name)
-            console.log(data.chain.evolves_to[0].evolves_to[0].species.name)
-            setEvolve(data.chain.evolves_to[0].species.name)
-            setEvolve(evolve => [...evolve, data.chain.evolves_to[0].evolves_to[0].species.name])
-            console.log(evolve)
+        response.json().then(function (data) {
+          //console.log(data);
+          console.log(data.chain.evolves_to[0].species.name);
+          console.log(data.chain.evolves_to[0].evolves_to[0].species.name);
+          const firstEvolve = data.chain.evolves_to[0].species.name;
+          const secondEvolve =
+            data.chain.evolves_to[0].evolves_to[0].species.name;
+          const evolvesInto = [firstEvolve, secondEvolve];
+          console.log(evolvesInto);
+          setEvolve(evolvesInto);
         });
-      }
-    )
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
-}
+      })
+      .catch(function (err) {
+        console.log("Fetch Error :-S", err);
+      });
+  };
 
-// const evolutions = evolve.map((item, index) =>
-//     <p key={index}>{item}</p>
-// );
+  const evolutions = evolve.map((item, index) => (
+    <p className={styles.evolutionName} key={index}>
+      {item}
+    </p>
+  ));
 
-  return (
-    
-    <div>
-        {/* {evolutions} */}
-    </div>
-
-  )
+  return <div>{evolutions}</div>;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,79 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import styles from '../css/styles.module.css';
-import Evolution from '../components/evolution';
-import Head from 'next/head'
-
+import React, { useState, useEffect } from "react";
+import styles from "../css/styles.module.css";
+import Evolution from "../components/evolution";
+import Head from "next/head";
 
 export default function Home() {
-
-  const [name, setName] = useState('mudkip');
+  const [name, setName] = useState("mudkip");
 
   const [id, setId] = useState(0);
 
-  const [sprite, setSprite] = useState('');
+  const [sprite, setSprite] = useState("");
 
-  const [spriteShiny, setSpriteShiny] = useState('');
+  const [spriteShiny, setSpriteShiny] = useState("");
 
   const [abilities, setAbilities] = useState([]);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/' + name)
-    .then(
-      function(response) {
+    fetch("https://pokeapi.co/api/v2/pokemon/" + name)
+      .then(function (response) {
         if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
+          console.log(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
           return;
         }
 
-        response.json().then(function(data) {
-          setName(data.name)
+        response.json().then(function (data) {
+          setName(data.name);
 
-          console.log(data.id)
+          console.log(data.id);
 
-          setId(data.id)
+          setId(data.id);
 
-          setSprite(data.sprites.front_default)
+          setSprite(data.sprites.front_default);
 
-          setSpriteShiny(data.sprites.front_shiny)
+          setSpriteShiny(data.sprites.front_shiny);
 
-          setAbilities(data.abilities)
+          setAbilities(data.abilities);
         });
-      }
-    )
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
+      })
+      .catch(function (err) {
+        console.log("Fetch Error :-S", err);
+      });
   }, []);
 
-  const abilitiesList = abilities.map((item, index) =>
+  const abilitiesList = abilities.map((item, index) => (
     <li key={index}>{item.ability.name}</li>
-  );
+  ));
 
   return (
     <>
-
       <Head>
         <title>Pokemon API testing</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
       <div className={styles.mainContainer}>
-
         <h1 className={styles.pokeName}>{name}</h1>
 
         <div className={styles.spriteContainer}>
+          <img style={{ width: "200px" }} src={sprite} />
 
-          <img style={{width: "200px"}} src={sprite} />
-
-          <img style={{width: "200px"}} src={spriteShiny} />
-
+          <img style={{ width: "200px" }} src={spriteShiny} />
         </div>
 
         <ul className={styles.abilityList}>{abilitiesList}</ul>
 
         <Evolution pokemonId={id} />
-
       </div>
 
       <style jsx global>{`
@@ -91,5 +83,5 @@ export default function Home() {
         }
       `}</style>
     </>
-  )
+  );
 }
