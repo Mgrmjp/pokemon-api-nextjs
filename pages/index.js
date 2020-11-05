68bf1e580bc0b2eae22e7886d740e61249def76e
Miikka <miikka.makela@zaibatsu.fi>
2020-11-05 14:28:11 +0200

    Struggling with type background coloring conditionally.

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Evolution from "../components/evolution";
 import Head from "next/head";
 
 export default function Home() {
-  const [name, setName] = useState("meowth");
+  const [name, setName] = useState("magikarp");
 
   const [id, setId] = useState(0);
 
@@ -13,6 +13,10 @@ export default function Home() {
   const [spriteShiny, setSpriteShiny] = useState("");
 
   const [abilities, setAbilities] = useState([]);
+
+  const [type, setType] = useState("");
+
+  const [color, setColor] = useState("");
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon/" + name)
@@ -27,7 +31,7 @@ export default function Home() {
         response.json().then(function (data) {
           setName(data.name);
 
-          console.log(data.id);
+          console.log(data);
 
           setId(data.id);
 
@@ -36,7 +40,17 @@ export default function Home() {
           setSpriteShiny(data.sprites.front_shiny);
 
           setAbilities(data.abilities);
+
+          setType(data.types[0].type.name);
         });
+
+        console.log(type);
+
+        if (type.length > 0) {
+          if (type == "water") {
+            setColor("#0b7fbd");
+          }
+        }
       })
       .catch(function (err) {
         console.log("Fetch Error :-S", err);
@@ -58,10 +72,16 @@ export default function Home() {
         <div className={styles.mainContainer}>
           <h1 className={styles.pokeName}>{name}</h1>
 
-          <div className={styles.spriteContainer}>
-            <img style={{ width: "200px" }} src={sprite} />
+          <h1 style={{ backgroundColor: color }} className={styles.pokemonType}>
+            {type}
+          </h1>
 
-            <img style={{ width: "200px" }} src={spriteShiny} />
+          <div className={styles.spriteRow}>
+            <div className={styles.spriteContainer}>
+              <img style={{ width: "250px" }} src={sprite} />
+
+              <img style={{ width: "250px" }} src={spriteShiny} />
+            </div>
           </div>
 
           <ul className={styles.abilityList}>{abilitiesList}</ul>
